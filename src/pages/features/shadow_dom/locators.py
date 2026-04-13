35b755d0dacd9ae67bccc100a2aefb7f5d52b39d
Avshalom Begler <avshalom210@gmail.com>
2026-04-13 14:15:48 +0300

    feat(shadow-dom): remove unused shadow host locator and add visibility check method

--- src/pages/features/shadow_dom/locators.py
+++ src/pages/features/shadow_dom/locators.py
@@ -10,4 +10,3 @@ from src.pages.base.base_page import Locator
 class ShadowDomLocators:
     PAGE_LOADED_INDICATOR: Locator = {"selector": "my-paragraph", "by": By.CSS_SELECTOR}
     HEADING: Locator = {"selector": "#content h1", "by": By.CSS_SELECTOR}
-    SHADOW_HOST: Locator = {"selector": "my-paragraph", "by": By.CSS_SELECTOR}
